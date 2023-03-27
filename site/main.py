76e2484dc62f96ad94573995a91807c5b30b4499
kukencium <120657577+kukencium@users.noreply.github.com>
2023-03-27 22:54:32 +0300

    немного сайтов

--- site/main.py
+++ site/main.py
@@ -8,12 +8,37 @@ def main_page():
     return render_template('main_page.html')
 
 
-@app.route('/test')
-def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title='ТЕСТОВАЯ СТРАНИЦА',
-                           place='Имя места', description='Описание места',
+@app.route('/тест')
+def test():
+    return render_template('index.html',
+                           title='ТЕСТОВАЯ СТРАНИЦА',
+                           place='Имя места',
+                           description='Описание места',
                            image=url_for('static', filename='test.png'))
+
+
+@app.route('/ТЦ_Троицк')
+def tc_troitsk():
+    shops = ['Автомойка', 'Pizza House', 'Mama SHU', 
+             'Белорусская лавка', 'Ресторан В Старом Городе','Перекрёсток', 
+             'Rusalut', 'Книжный интернет-магазин', 'Барбершоп BORODACH']
+    
+    return render_template('index.html',
+                           title='ТЦ Троицк',
+                           place='ТЦ Троицк',
+                           description='Самый большой торговый центр в Троицке. Список его заведений:',
+                           in_place=shops,
+                           image=url_for('static', filename='ТЦ Троицк.png'))
+
+
+@app.route('/Заречье')
+def zarechye():
+    return render_template('index.html',
+                           title='Заречье',
+                           place='Заречье',
+                           description='Самый большой парк Троицка. На территории можно арендовать беседки и'
+                                       ' покушать в одноимённом ресторане.',
+                           image=url_for('static', filename='Заречье.png'))
 
 
 if __name__ == '__main__':
