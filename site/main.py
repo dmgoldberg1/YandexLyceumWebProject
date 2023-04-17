24c5a6236554183414eaa900399a601f5f1a6b2e
kukencium <120657577+kukencium@users.noreply.github.com>
2023-04-17 17:48:33 +0300

    +300 :)

--- site/main.py
+++ site/main.py
@@ -23,113 +23,342 @@ def test():
                            image=url_for('static', filename='test.png'))
 
 
-@app.route('/ТЦ_Троицк')
-def tc_troitsk():
-    shops = ['Автомойка', 'Pizza House', 'Mama SHU',
-             'Белорусская лавка', 'Ресторан В Старом Городе', 'Перекрёсток',
-             'Rusalut', 'Книжный Лабиринт', 'BORODACH',
-             'До Рассвета', 'М1', 'Кондитерская',
-             'Батарейки', 'Шведская чистка', 'Эрика',
-             'FUN&SUN', 'Экспедиция', 'Картинная галерея',
-             'Садива', 'Chimp Champ', 'Maxx',
-             'ProfLine', 'Yagona', 'Смарт Скул',
-             'Школа самбо и дзюдо', 'Электроник', 'Ермолино',
-             'Титан', 'Ozon', 'Вояж',
-             'Free Move Family', 'АльфаСтрахование',
-             'Мои небеса', 'Мирабела', 'Академия развития Бамбино',
-             'Iva Beauty Lab', 'Торты', 'Msk-stone',
-             'АвтоАкадемия', 'Техномания', 'Boxberry']
-
+# Нет описания
+# Нет фотографии
+@app.route('/Волейбол')
+def voleybol():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
     return render_template('index.html',
-                           title='ТЦ Троицк',
-                           place='Торговый Центр Троицк',
-                           description='Самый большой торговый центр в Троицке. Список его заведений:',
-                           in_place=shops,
-                           image=url_for('static', filename='ТЦ Троицк.png'))
+                           title='Волейбол',
+                           place='Волейбол',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
 
 
+# Нет описания
+# Нет фотографии
+@app.route('/Футбол')
+def futbol():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Футбол',
+                           place='Футбол',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Лодочки')
+def lodochki():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Лодочки',
+                           place='Лодочки',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/МАУК_Троицкая_библиотека_№_1_имени_Михайловых')
+def mauk_troitskaya_biblioteka_n_1_imeni_mikhaylovikh():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='МАУК Троицкая библиотека № 1 имени Михайловых',
+                           place='МАУК Троицкая библиотека № 1 имени Михайловых',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Спортивно-оздоровительная_база_Лесная')
+def sportivno_ozdorovitelnaya_baza_lesnaya():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Спортивно-оздоровительная база Лесная',
+                           place='Спортивно-оздоровительная база Лесная',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Троицкие_бани')
+def troitskie_bani():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Троицкие бани',
+                           place='Троицкие бани',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Дом_книги')
+def dom_knigi():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Дом книги',
+                           place='Дом книги',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Троицкая_библиотека_№_2')
+def troitskaya_biblioteka_n_2():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Троицкая библиотека № 2',
+                           place='Троицкая библиотека № 2',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Театр-студия_17')
+def teatr_studiya_17():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Театр-студия 17',
+                           place='Театр-студия 17',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/додо_пицца')
+def dodo_pitstsa():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='додо пицца',
+                           place='додо пицца',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/YourTime')
+def yourtime():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='YourTime',
+                           place='YourTime',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/В_старом_городе')
+def v_starom_gorode():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='В старом городе',
+                           place='В старом городе',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
 @app.route('/Заречье')
-def zarechye():
+def zareche():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
     return render_template('index.html',
                            title='Заречье',
-                           place='Парк Заречье',
-                           description='Самый большой парк Троицка. На территории можно арендовать беседку или'
-                                       ' покушать в одноимённом ресторане.',
-                           image=url_for('static', filename='Заречье.png'))
+                           place='Заречье',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
 
 
-@app.route('/Квант')
-def kvant():
-    shops = ['Чемпион', 'Дэма', 'ТШСВиЕ Белый Тигр']
-
+# Нет описания
+# Нет фотографии
+@app.route('/Академический_сквер')
+def akademicheskiy_skver():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
     return render_template('index.html',
-                           title='Квант',
-                           place='Дворец спорта Квант',
-                           description='Центр спорта Троицка. Предлагает большое разнообразие секций.',
-                           in_place=shops,
-                           image=url_for('static', filename='Квант.png'))
+                           title='Академический сквер',
+                           place='Академический сквер',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
 
 
-@app.route('/Рынок')
-def rinok():
-    shops = ['Металлоремонт', 'Продукты Ермолино', 'Обувь',
-             'Ozon', 'DPD', 'Boxberry'
-             'Яндекс Маркет', 'IML']
-
+# Нет описания
+# Нет фотографии
+@app.route('/Октябрьский_проспект')
+def oktyabrskiy_prospekt():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
     return render_template('index.html',
-                           title='Рынок',
-                           place='Центральный Рынок',
-                           description='Главный рынок Троицка.',
-                           in_place=shops,
-                           image=url_for('static', filename='Рынок.png'))
+                           title='Октябрьский проспект',
+                           place='Октябрьский проспект',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
 
 
-@app.route('/Баня')
-def banya():
-    shops = ['Т-Клуб', 'Отопление-Троицк', 'Чемпионика',
-             'Araz RichMan', 'Буханка', 'МУП Троицкие городские бани',
-             'Троицкие сауны', 'Грифон', 'Небеса',
-             'Заречье', 'Арарата денс']
-
+# Нет описания
+# Нет фотографии
+@app.route('/Усадьба_Троицкое')
+def usadba_troitskoe():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
     return render_template('index.html',
-                           title='Баня',
-                           place='Троицкие бани',
-                           description='Небезызвестные бани Троицка.',
-                           in_place=shops,
-                           image=url_for('static', filename='Баня.png'))
+                           title='Усадьба Троицкое',
+                           place='Усадьба Троицкое',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
 
 
-@app.route('/Вкусно_И_Точка')
-def macdonalds():
+# Нет описания
+# Нет фотографии
+@app.route('/Троицкий_музей_имени_М._Н._Лялько')
+def troitskiy_muzey_imeni_m_n_lyalko():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
     return render_template('index.html',
-                           title='Вкусно И Точка',
-                           place='Вкусно И Точка',
-                           description='Бывший Макдональдс. Самый забитый ресторан быстрого питания в Троицке.',
-                           image=url_for('static', filename='Макдональдс.png'))
+                           title='Троицкий музей имени М. Н. Лялько',
+                           place='Троицкий музей имени М. Н. Лялько',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
 
 
-@app.route('/Додо_Пицца')
-def dodo():
+# Нет описания
+# Нет фотографии
+@app.route('/Храм_Живоначальной_Троицы_в_Троицке')
+def khram_jivonachalnoy_troitsi_v_troitske():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
     return render_template('index.html',
-                           title='Додо',
-                           place='Додо Пицца',
-                           description='Самая лучшая пиццерия в городе. За приличную цену приличная еда.',
-                           image=url_for('static', filename='Додо Пицца.png'))
+                           title='Храм Живоначальной Троицы в Троицке',
+                           place='Храм Живоначальной Троицы в Троицке',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
 
 
-@app.route('/Изумрудный_Город')
-def izumrudniy_gorod():
-    shops = ['Изумрудный город', 'С-Логика', 'Fix Price',
-             'FoodBand.ru', 'ГлавБухъ', 'Пятёрочка',
-             'Женская одежда', 'Пивмаг', 'DesignStudio22',
-             'Трикотаж', 'Косметология']
-
+# Нет описания
+# Нет фотографии
+@app.route('/d')
+def d():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
     return render_template('index.html',
-                           title='Изумрудный Город',
-                           place='Изумрудный Город',
-                           description='Пользуется популярностью среди местных школьников.',
-                           in_place=shops,
-                           image=url_for('static', filename='Изумрудный Город.png'))
+                           title='d',
+                           place='d',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Младший_научный_сотрудник')
+def mladshiy_nauchniy_sotrudnik():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Младший научный сотрудник',
+                           place='Младший научный сотрудник',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Троицкий_Дом_ученых')
+def troitskiy_dom_uchenikh():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Троицкий Дом ученых',
+                           place='Троицкий Дом ученых',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Вантовый_мост')
+def vantoviy_most():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Вантовый мост',
+                           place='Вантовый мост',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Церковь_Тихвинской_иконы_Божией_Матери')
+def tserkov_tikhvinskoy_ikoni_bojiey_materi():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Церковь Тихвинской иконы Божией Матери',
+                           place='Церковь Тихвинской иконы Божией Матери',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Огонь_памяти')
+def ogon_pamyati():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Огонь памяти',
+                           place='Огонь памяти',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Культурно-технический_центр_Профкома_Тринити')
+def kulturno_tekhnicheskiy_tsentr_profkoma_triniti():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Культурно-технический центр Профкома Тринити',
+                           place='Культурно-технический центр Профкома Тринити',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
+
+
+# Нет описания
+# Нет фотографии
+@app.route('/Физическая_кунсткамера')
+def fizicheskaya_kunstkamera():
+    with open('descriptions/Нет описания.txt', 'r', encoding='utf8') as fp:
+        description = fp.readline()
+    return render_template('index.html',
+                           title='Физическая кунсткамера',
+                           place='Физическая кунсткамера',
+                           description=description,
+                           image=url_for('static', filename='Нет картинки.png'))
 
 
 if __name__ == '__main__':
