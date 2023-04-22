17d2a3a7f5c1206412f66958015069d4ba1b5bdb
kukencium <120657577+kukencium@users.noreply.github.com>
2023-04-22 13:10:57 +0300

    Update main.py

--- site/main.py
+++ site/main.py
@@ -4,27 +4,37 @@ import sqlite3
 
 app = Flask(__name__)
 
+COLOURS = {'activity': '#D1E0FF',
+           'food': '#FAECE5',
+           'walk': '#EFFACE'}
 
 @app.route('/')
 def main_page():
     base = 'http://127.0.0.1:8080/'
     links = []
-
-    db_path = os.path.abspath('code_printer.py').split('\_'[0])[:-2] + ['bot', 'trobot.db']
-
+    db_path = os.path.abspath('main.py').split('\_'[0])[:-2] + ['bot', 'ex.db']
     con = sqlite3.connect('\_'[0].join(db_path))
     cur = con.cursor()
 
+    links.append(('Где можно провести досуг:', COLOURS['activity']))
     result = cur.execute("""SELECT name FROM activity""").fetchall()
-    result += cur.execute("""SELECT name FROM food""").fetchall()
-    result += cur.execute("""SELECT name FROM walk""").fetchall()
-
-    con.close()
-
     for i in result:
         links.append(i[0])
 
+    links.append(('Где можно поесть:',  COLOURS['food']))
+    result = cur.execute("""SELECT name FROM food""").fetchall()
+    for i in result:
+        links.append(i[0])
+
+    links.append(('Где можно прогуляться:', COLOURS['walk']))
+    result = cur.execute("""SELECT name FROM walk""").fetchall()
+    for i in result:
+        links.append(i[0])
+
+    con.close()
+
     return render_template('main_page.html',
+                           logo=url_for('static', filename='Логотип.png'),
                            base=base,
                            links=links)
 
@@ -40,36 +50,39 @@ def test():
 
 @app.route('/<place>')
 def sites(place):
-    db_path = os.path.abspath('code_printer.py').split('\_'[0])[:-2] + ['bot', 'ex.db']
+    db_path = os.path.abspath('main.py').split('\_'[0])[:-2] + ['bot', 'ex.db']
 
     con = sqlite3.connect('\_'[0].join(db_path))
     cur = con.cursor()
 
-    result = cur.execute("""SELECT name FROM activity WHERE name = ?""", (place,)).fetchall()
-    result += cur.execute("""SELECT name FROM food WHERE name = ?""", (place,)).fetchall()
-    result += cur.execute("""SELECT name FROM walk WHERE name = ?""", (place,)).fetchall()
-    print(result)
+    background = '#BAB1ED'
+
+    result = cur.execute("""SELECT name, place, desc, pict FROM activity WHERE name = ?""", (place,)).fetchall()
+    if result:
+        background = COLOURS['activity']
+
+    result += cur.execute("""SELECT name, place, desc, pict FROM food WHERE name = ?""", (place,)).fetchall()
+    if result and background == '#BAB1ED':
+        background = COLOURS['food']
+
+    result += cur.execute("""SELECT name, place, desc, pict FROM walk WHERE name = ?""", (place,)).fetchall()
+    if result and background == '#BAB1ED':
+        background = COLOURS['walk']
 
     con.close()
 
     if result:
-        description = 'descriptions/' + place + '.txt'
-        photo = place + '.png'
-
-        if not os.path.exists(description):
-            print('# Нет описания')
-            description = 'descriptions/Нет описания.txt'
-        if not os.path.exists(photo):
-            print('# Нет фотографии')
-            photo = 'Нет картинки.png'
-
+        photo = url_for('static', filename=result[0][3])
         return render_template('index.html',
                                title=result[0][0],
-                               place=result[0][0],
-                               description='Описание места',
-                               image=url_for('static', filename='test.png'))
+                               name=result[0][0],
+                               place=result[0][1],
+                               description=result[0][2],
+                               background=background,
+                               image='/'.join(photo.split('%5C')))
 
     else:
+        print(url_for('static', filename='Нет картинки.png'))
         return render_template('non_existent.html')
 
 
@@ -79,4 +92,4 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='192.168.68.125')
+    app.run(port=8080, host='127.0.0.1')
